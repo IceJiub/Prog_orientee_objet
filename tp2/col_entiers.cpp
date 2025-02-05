597441e856b57a4b6ddbd8c638aacb6ee0b75b70
IceJiub <tom.yang@hotmail.fr>
2025-02-05 13:39:01 +0100

    Add Col_Entiers class with constructor for dynamic integer array

---
+++ tp2/col_entiers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+#include "col_entiers.hpp"
+using namespace std;
+
+Col_Entiers(int taille)
+{
+    this->taille = taille;
+    this->nbe = 0;
+    this->T = new int[taille];
+}
---
+++ tp2/col_entiers.hpp
@@ -0,0 +1,14 @@
+#ifndef COL_ENTIERS_HPP
+#define COL_ENTIERS_HPP
+
+class Col_Entiers
+{
+    int * T;
+    int nbe;
+    int taille;
+    
+    public:
+        Col_Entiers(int taille);
+};
+
+#endif
