5dda19a6c18939d5fabbb5dcb4e9faf2c894f3bb
IceJiub <tom.yang@hotmail.fr>
2025-02-11 19:26:00 +0100

    Refactor main function to utilize constructor and improve testing of Liste class methods

--- tp3/main.cpp
+++ tp3/main.cpp
@@ -5,29 +5,46 @@ using namespace std;
 
 int main()
 {
-    Liste *l = new Liste();
-    (*l).ajout_queue(1);
-    (*l).ajout_queue(2);
-    (*l).ajout_queue(3);
-    (*l).ajout_queue(4);
-    (*l).ajout_queue(5);
-    (*l).afficher();
-    (*l).afficher_Rec((*l).getTete());
-    (*l).afficher_Inverse((*l).getTete());
-    cout << "Cardinal : " << (*l).cardinal() << endl;
-    cout << "Chercher 3 : " << (*l).chercher(3) << endl;
-    cout << "Chercher 6 : " << (*l).chercher(6) << endl;
-    (*l).ajout_queue(5);
-    (*l).afficher();
-    cout << "Nb occurences de 5 : " << (*l).Nb_Occurences(5) << endl;
-    cout << "Nb occurences de 6 : " << (*l).Nb_Occurences(6) << endl;
-    (*l).afficher();
-    (*l).supprimer(5);
-    (*l).afficher();
-    (*l).supprimer(1);
-    (*l).afficher();
-    (*l).supprimer(1);
-    (*l).supprimer_Tout();
-    (*l).afficher();
-    delete l;
+    int tab[10] = {1,2,3,4,5,6,7,8,9,0};
+    Liste L(tab,10);
+
+    // Tests des fonctions d'affichages
+    L.afficher();
+    L.afficher_Rec(L.getTete());
+    L.afficher_Inverse(L.getTete());
+
+    // Test cardinal()
+    cout << "Nombre d'éléments de la liste : " << L.cardinal() << endl;
+
+    // Test chercher()
+    cout << "Recherche 1 : " << L.chercher(1) << endl;
+    cout << "Recherche 99 : " << L.chercher(99) << endl;
+
+    // Test nb_Occurence()
+    cout << "Recherche occurence 5 : " << L.nb_Occurence(5) << endl;
+    L.ajout_queue(5);
+    L.afficher();
+    cout << "Recherche occurence 5 : " << L.nb_Occurence(5) << endl;
+
+    // Test supprimer()
+    cout << "Suppression 5" << endl;
+    L.supprimer(5);
+    L.afficher();
+    cout << "Suppression 99" << endl;
+    L.supprimer(99);
+    cout << "Suppression 1" << endl;
+    L.supprimer(1);
+    L.afficher();
+
+    // Test supprimer_tout()
+    L.ajout_queue(4);
+    L.ajout_queue(4);
+    L.ajout_queue(4);
+    L.afficher();
+    L.supprimer_Tout(4);
+    L.afficher();
+    
+    // Test du constructeur par copie
+    Liste L1(L);
+    L1.afficher();
 }
